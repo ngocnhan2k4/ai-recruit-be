3f56c826666e004739e080fd7f92f024e98ef27c
tranngocnhan <tranngocnhannt2004@gmail.com>
2025-09-27 10:58:29 +0700

    add get me api

--- src/interfaces/controllers/user.controller.ts
+++ src/interfaces/controllers/user.controller.ts
@@ -4,11 +4,14 @@ import {
   UseGuards,
   Param,
   ParseIntPipe,
+  Req,
 } from "@nestjs/common";
 import { UserUseCases } from "src/use-cases/user/user.use-case";
 import { JwtAuthGuard } from "@/frameworks/auth-services/guards";
 //import { CasbinPermission } from "@/frameworks/auth-services/casbin/casbin.decorator";
 import { ApiResponse, GetUserDto } from "@/interfaces/dtos";
+import { FastifyRequest } from "fastify";
+import { TokenPayload } from "@/common/types/token";
 import {
   ApiTags,
   ApiOperation,
@@ -101,5 +104,71 @@ export class UserController {
   @Get(":id")
   async getById(@Param("id", ParseIntPipe) id: number) {
     return this.userUseCases.getUserById(id);
+  }
+
+  @ApiOperation({
+    summary: "Get current user",
+    description: "Retrieve information about the currently authenticated user.",
+  })
+  @ApiOkResponse({
+    description: "Retrieve user success",
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponse) },
+        {
+          properties: {
+            data: { $ref: getSchemaPath(GetUserDto) },
+          },
+        },
+      ],
+      example: {
+        code: "SUCCESS",
+        message: "Success",
+        data: {
+          id: 1,
+          username: "exampleuser",
+          email: "user@example.com",
+          phone: "1234567890",
+          avatarUrl: "https://cdn.example.com/avatar.png",
+          name: "John Doe",
+          dob: "1990-01-01",
+          gender: "Male",
+          firebaseUid: "lPuOOqhJlsc8J5Va7Jg2cYNMp323",
+        },
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: "User not found",
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponse) },
+        {
+          example: {
+            code: "USER_NOT_FOUND",
+            message: "User not found.",
+          },
+        },
+      ],
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: "Invalid or missing access token",
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponse) },
+        {
+          example: {
+            code: 401,
+            message: "Unauthorized",
+            stack: "...",
+          },
+        },
+      ],
+    },
+  })
+  @Get("me")
+  getMe(@Req() req: FastifyRequest & { user: TokenPayload }) {
+    return this.userUseCases.getUserByAccessToken(req.user);
   }
 }
--- src/use-cases/user/user.use-case.ts
+++ src/use-cases/user/user.use-case.ts
@@ -4,6 +4,7 @@ import { User } from "../../core/entities";
 import { IDataServices } from "../../core/abstracts";
 import { UserFactoryService } from "./user-factory.service";
 import { RESPONSE_CODE, RESPONSE_MESSAGE } from "@/common/constants/response";
+import { TokenPayload } from "@/common/types/token";
 
 @Injectable()
 export class UserUseCases {
@@ -17,6 +18,27 @@ export class UserUseCases {
   }
 
   async getUserById(id: number): Promise<ApiResponse<GetUserDto>> {
+    const user: User | null = await this.dataServices.users.get(id);
+    if (!user) {
+      throw new NotFoundException(
+        new ApiResponse({
+          message: RESPONSE_MESSAGE.USER_NOT_FOUND,
+          code: RESPONSE_CODE.USER_NOT_FOUND,
+        }),
+      );
+    }
+    const userDto = GetUserDto.from(user);
+    return new ApiResponse<GetUserDto>({
+      message: RESPONSE_MESSAGE.SUCCESS,
+      code: RESPONSE_CODE.SUCCESS,
+      data: userDto,
+    });
+  }
+
+  async getUserByAccessToken(
+    payload: TokenPayload,
+  ): Promise<ApiResponse<GetUserDto>> {
+    const id: number = payload.sub;
     const user: User | null = await this.dataServices.users.get(id);
     if (!user) {
       throw new NotFoundException(
