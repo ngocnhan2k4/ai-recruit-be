4bae1331480dedaebef251482ff9eb5af55fb8a0
nhattran04 <nhattran@phygitallabs.io>
2025-10-26 15:54:36 +0700

    fix: ci

--- src/interfaces/dtos/users/user.dto.ts
+++ src/interfaces/dtos/users/user.dto.ts
@@ -160,7 +160,7 @@ export class GetUserResponseDto {
   provider: ProviderEnum;
 
   @Expose()
-  onboardingCompleted: boolean;
+  onboardingCompleted: boolean | undefined;
 
   static from(partial: Partial<GetUserResponseDto>) {
     return plainToInstance(GetUserResponseDto, partial, {
