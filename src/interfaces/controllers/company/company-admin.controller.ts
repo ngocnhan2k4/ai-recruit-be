a7a26740d39afc4a8ae3b38e0b19546ab7c00389
nhattran04 <nhattran@phygitallabs.io>
2025-11-08 11:05:29 +0700

    add guard authorize

--- src/interfaces/controllers/company/company-admin.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { CompanyUseCase } from "@/use-cases/company/company.use-case";
-
-@ApiTags("Companies Admin")
-@Controller("admin/companies")
-export class CompanyAdminController {
-  constructor(private readonly companyUseCase: CompanyUseCase) {}
-}
--- src/interfaces/controllers/organization/admin-organization.controller.ts
+++ src/interfaces/controllers/organization/admin-organization.controller.ts
@@ -2,11 +2,11 @@ import { OrganizationUseCase } from "@/use-cases/organization/organization.use-c
 import { Controller, UseGuards } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "@/frameworks/auth-services/guards/jwt-auth.guard";
-import { OrganizationAuthorizeGuard } from "@/frameworks/auth-services/guards/organization-authorize.guard";
+import { SystemAuthorizeGuard } from "@/frameworks/auth-services/guards";
 
 @ApiTags("Organization Admin")
 @Controller("organizations/:orgId")
-@UseGuards(JwtAuthGuard, OrganizationAuthorizeGuard)
+@UseGuards(JwtAuthGuard, SystemAuthorizeGuard)
 export class OrganizationAdminController {
   constructor(private readonly organizationUseCase: OrganizationUseCase) {}
 }
