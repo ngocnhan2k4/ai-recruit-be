d1d11e90d681d3247c7ac0bd9bf333a59ef38d1b
nhankhtn <nguyenanhnguyen3006@gmail.com>
2025-12-26 23:31:20 +0700

    update connect redis

--- src/frameworks/message-queue/message-queue.module.ts
+++ src/frameworks/message-queue/message-queue.module.ts
@@ -1,13 +1,23 @@
 import { Module } from "@nestjs/common";
-import { RedisModule } from "../redis/redis.module";
 import { MessageQueueService } from "./message-queue.service";
 import { IMessageQueueService } from "@/core/abstracts/message-queue.abstract";
 import { BullModule } from "@nestjs/bullmq";
 import { JOB_INDEX_QUEUE } from "@/common/constants/queue";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    RedisModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>("REDIS_HOST"),
+          port: configService.get<number>("REDIS_PORT"),
+          password: configService.get<string>("REDIS_PASSWORD"),
+        },
+      }),
+      inject: [ConfigService],
+    }),
     BullModule.registerQueue({
       name: JOB_INDEX_QUEUE,
     }),
