bd44974bb7d68d69a0ebf5723ee4314e6265ff31
ngocfat <ngocphatc2710@gmail.com>
2025-12-30 16:10:47 +0700

    update db index

--- src/frameworks/data-services/postgres/models/refresh-token.model.ts
+++ src/frameworks/data-services/postgres/models/refresh-token.model.ts
@@ -5,17 +5,26 @@ import {
   varchar,
   boolean,
   uuid,
+  uniqueIndex,
+  index,
 } from "drizzle-orm/pg-core";
 
 import { users } from "./user.model";
 
-export const refreshTokens = pgTable("refresh_tokens", {
-  id: serial("id").primaryKey(),
-  userId: uuid("user_id")
-    .notNull()
-    .references(() => users.id),
-  token: varchar("token").notNull(),
-  expiresAt: timestamp("expires_at").notNull(),
-  revoked: boolean("revoked").notNull().default(false),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+export const refreshTokens = pgTable(
+  "refresh_tokens",
+  {
+    id: serial("id").primaryKey(),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => users.id),
+    token: varchar("token").notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+    revoked: boolean("revoked").notNull().default(false),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => [
+    uniqueIndex("idx_refresh_tokens_token").on(table.token),
+    index("idx_refresh_tokens_user").on(table.userId),
+  ],
+);
