4dd7f298849963592a6e6c5589544075dd9948a0
tranngocnhan <tranngocnhannt2004@gmail.com>
2026-02-27 21:26:01 +0700

    refactor(province): remove isNew field and simplify getAllProvinces query

--- src/frameworks/data-services/postgres/models/province.model.ts
+++ src/frameworks/data-services/postgres/models/province.model.ts
@@ -1,7 +1,6 @@
-import { uuid, pgTable, varchar, boolean } from "drizzle-orm/pg-core";
+import { uuid, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const provinces = pgTable("provinces", {
   id: uuid("id").defaultRandom().primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
-  isNew: boolean("is_new").notNull().default(false),
 });
