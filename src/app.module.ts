87774faf5fa0316abd1777210ef7dfd523abff9a
tienminhktvn <caotienminhktvn3@gmail.com>
2026-03-02 20:42:03 +0700

    remove RedisModule import comment from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,7 +38,6 @@ import { CasbinModule } from "./frameworks/auth-services/casbin/casbin.module";
 import { JwtStrategy } from "./frameworks/auth-services/strategies/jwt.strategy";
 import { CategoryUseCasesModule } from "./use-cases/category/category-use-cases.module";
 import { JobUseCasesModule } from "./use-cases/job/job-use-cases.module";
-//import { RedisModule } from "./frameworks/redis/redis.module";
 import { CloudinaryModule } from "./frameworks/storage/cloudinary/cloudinary.module";
 import { StorageModule } from "./use-cases/storage/storage.module";
 import { TerminusModule } from "@nestjs/terminus";
@@ -84,7 +83,6 @@ import { AiCvUseCasesModule } from "./use-cases/ai-cv/ai-cv.use-cases.module";
       validate: validateConfig,
     }),
     ScheduleModule.forRoot(),
-    //RedisModule,
     UserUseCasesModule,
     JobUseCasesModule,
     AuthUseCasesModule,
