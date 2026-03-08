4384773de4f842cd3c72c7a77216fc13eb186001
tienminhktvn <caotienminhktvn3@gmail.com>
2026-03-08 16:01:33 +0700

    fix(rate-limit): fix rate limit which allow auth check and token refresh

--- src/app.module.ts
+++ src/app.module.ts
@@ -188,6 +188,9 @@ import { RateLimitMiddleware } from "./common/middlewares";
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RateLimitMiddleware).exclude("/health").forRoutes("*");
+    consumer
+      .apply(RateLimitMiddleware)
+      .exclude("/health", "users/me", "auth/refresh")
+      .forRoutes("*");
   }
 }
