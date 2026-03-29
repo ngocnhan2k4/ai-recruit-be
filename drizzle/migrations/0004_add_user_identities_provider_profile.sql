2806033cf18f84513aeda7d397084288ebd5a36f
ngocfat <ngocphatc2710@gmail.com>
2026-03-30 00:34:30 +0700

    chore

--- drizzle/migrations/0004_add_user_identities_provider_profile.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "user_identities"
-  ADD COLUMN IF NOT EXISTS "provider_email" varchar(255),
-  ADD COLUMN IF NOT EXISTS "provider_name" varchar(255),
-  ADD COLUMN IF NOT EXISTS "provider_picture" varchar(500);
