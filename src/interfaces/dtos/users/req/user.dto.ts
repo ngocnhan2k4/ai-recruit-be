3eee1927eb0d99833efc2de7f48e41d4d37100e1
nhankhtn <nguyenanhnguyen3006@gmail.com>
2026-04-04 21:18:32 +0700

    refactor api admin user

--- src/interfaces/dtos/users/req/user.dto.ts
+++ src/interfaces/dtos/users/req/user.dto.ts
@@ -7,10 +7,11 @@ import {
   IsString,
   IsArray,
   IsNumber,
+  IsUUID,
 } from "class-validator";
-import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional, PartialType } from "@nestjs/swagger";
 import { GeneralQueryDto } from "../../common/query";
-import { GenderEnum } from "@/core";
+import { GenderEnum, UserSubscriptionStatusEnum } from "@/core";
 import { RoleEnum } from "@/common/constants";
 
 export class CreateUserRequestDto {
@@ -115,6 +116,22 @@ export class GetUserQueryDto extends GeneralQueryDto {
   @IsOptional()
   @IsBoolean()
   isDeleted?: boolean;
+
+  @ApiPropertyOptional({
+    description: "Filter by subscription id",
+    example: "550e8400-e29b-41d4-a716-446655440001",
+  })
+  @IsOptional()
+  @IsUUID()
+  subscriptionId?: string;
+
+  @ApiPropertyOptional({
+    enum: UserSubscriptionStatusEnum,
+    description: "Filter by status",
+  })
+  @IsOptional()
+  @IsEnum(UserSubscriptionStatusEnum)
+  statusSubscription?: UserSubscriptionStatusEnum;
 }
 
 export class AdminUpdateUserRequestDto {
