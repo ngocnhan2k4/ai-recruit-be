f7d321484a7958bb01e93ed4f2f3008f0c31245d
nhankhtn <nguyenanhnguyen3006@gmail.com>
2026-04-06 23:06:20 +0700

    chore

--- src/interfaces/dtos/feedback/req/feedback.dto.ts
+++ src/interfaces/dtos/feedback/req/feedback.dto.ts
@@ -10,6 +10,7 @@ import {
   ValidateIf,
 } from "class-validator";
 import { FeedbackStatusEnum } from "@/core/entities/enum.entity";
+import { Type } from "class-transformer";
 
 export class CreateFeedbackRequestDto {
   @ApiProperty({ description: "Name of the user submitting feedback" })
@@ -51,11 +52,13 @@ export class GetFeedbacksRequestDto extends GeneralQueryDto {
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @Type(() => Date)
   @IsDate()
   startDate?: Date;
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @Type(() => Date)
   @IsDate()
   endDate?: Date;
 }
