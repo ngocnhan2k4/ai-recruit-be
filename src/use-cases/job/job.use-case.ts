88f1d984a1c289be83747a9d6542419da773a844
tienminhktvn <caotienminhktvn3@gmail.com>
2026-04-19 15:17:28 +0700

    feat(crawl_jobs): add job sync to es logic

--- src/use-cases/job/job.use-case.ts
+++ src/use-cases/job/job.use-case.ts
@@ -12,7 +12,6 @@ import {
   IUserRepository,
   INotificationRepository,
 } from "@/core/abstracts";
-import { IUserFeatureUsageRepository } from "@/core/abstracts/repositories/user-feature-usage-repository.abstract";
 import {
   ApiResponse,
   JobCountsDto,
@@ -87,7 +86,6 @@ export class JobUseCases {
     private readonly jobSearchService: IJobSearchService,
     private readonly notificationRepository: INotificationRepository,
     private readonly cvRepository: ICvRepository,
-    private readonly userFeatureUsageRepository: IUserFeatureUsageRepository,
     private readonly featureService: FeatureService,
   ) {}
 
